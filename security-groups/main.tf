b0d1eace4ffc44615ab3d1db5b9e13831fc68fd8
Tosin Ojo <ojotosin1900@gmail.com>
2023-04-05 21:51:01 -0400

    fixing security group cidr block

--- security-groups/main.tf
+++ security-groups/main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "alb_security_group" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -17,7 +17,7 @@ resource "aws_security_group" "alb_security_group" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
